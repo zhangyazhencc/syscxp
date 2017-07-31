94f26d088dd72fe8914a3ecaaa58166c9c50add9
赵相辉 <zhaoxh@syscloud.cn>
2017-07-31 16:08:58 +0800

    update

--- build/deploy.sh
+++ build/deploy.sh
@@ -10,9 +10,9 @@ cd ..
 mvn -DskipTests clean install
 cd -
 
-＃mvn war:war
-＃rm -rf $CATALINA_HOME/webapps/zstack
-＃rm -f $CATALINA_HOME/webapps/zstack.war
-＃cp target/zstack.war $CATALINA_HOME/webapps/zstack.war
+### mvn war:war
+### rm -rf $CATALINA_HOME/webapps/zstack
+### rm -f $CATALINA_HOME/webapps/zstack.war
+### cp target/zstack.war $CATALINA_HOME/webapps/zstack.war
 
 echo "Deployed zstack.war to $CATALINA_HOME/webapps/zstack.war, run 'mvn -pl build -P debug exec:exec -Ddebug' to start"
