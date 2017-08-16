994860912a53dd6175403723cba18e3d2a6777b4
wj5633 <wangjie5633@163.com>
2017-08-16 15:03:07 +0800

    notice

---
+++ account/src/main/java/org/zstack/account/header/log/APIDeleteNoticeMsg.java
@@ -0,0 +1,33 @@
+package org.zstack.account.header.log;
+
+import org.zstack.header.message.APIDeleteMessage;
+import org.zstack.header.message.APIEvent;
+import org.zstack.header.message.APIMessage;
+import org.zstack.header.message.APIParam;
+import org.zstack.header.notification.ApiNotification;
+
+public class APIDeleteNoticeMsg extends APIDeleteMessage{
+    @APIParam()
+    private String uuid;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public ApiNotification __notification__() {
+        APIMessage that = this;
+
+        return new ApiNotification() {
+            @Override
+            public void after(APIEvent evt) {
+                ntfy(String.format("Delete Notice[uuid: %s]", uuid)).resource(uuid, NoticeVO.class.getSimpleName())
+                        .messageAndEvent(that, evt).done();
+            }
+        };
+    }
+
+}
