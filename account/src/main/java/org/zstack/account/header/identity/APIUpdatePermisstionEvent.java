6b7c770f3e6da113e4f8669506d071c87f0c9b91
wangwg <wangwg@syscloud.cn>
2017-08-16 17:55:54 +0800

    update

--- account/src/main/java/org/zstack/account/header/identity/APIUpdatePermisstionEvent.java
+++ account/src/main/java/org/zstack/account/header/identity/APIUpdatePermisstionEvent.java
@@ -4,13 +4,10 @@ import org.zstack.header.message.APIEvent;
 import org.zstack.header.rest.RestResponse;
 
 @RestResponse(allTo = "inventory")
-<<<<<<< HEAD:account/src/main/java/org/zstack/account/header/identity/APIUpdatePermisstionEvent.java
+
+
 public class APIUpdatePermisstionEvent extends APIEvent {
-    private AuthorityInventory inventory;
-=======
-public class APIUpdateAuthorityEvent extends APIEvent {
     private PermissionInventory inventory;
->>>>>>> 4993a761587240fd1e76817a47f0a28c5c180319:account/src/main/java/org/zstack/account/header/identity/APIUpdateAuthorityEvent.java
 
     public APIUpdatePermisstionEvent(String apiId) {
         super(apiId);
