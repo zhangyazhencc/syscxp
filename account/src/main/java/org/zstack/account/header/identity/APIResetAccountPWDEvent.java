1b1b4847ee450f788b9396abbcb2b1a38d2987dd
wangwg <wangwg@syscloud.cn>
2017-08-22 17:32:51 +0800

    add

---
+++ account/src/main/java/org/zstack/account/header/identity/APIResetAccountPWDEvent.java
@@ -0,0 +1,23 @@
+package org.zstack.account.header.identity;
+
+import org.zstack.header.message.APIEvent;
+
+public class APIResetAccountPWDEvent extends APIEvent {
+    private String password;
+
+    public APIResetAccountPWDEvent(String apiId) {
+        super(apiId);
+    }
+
+    public APIResetAccountPWDEvent() {
+        super(null);
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
