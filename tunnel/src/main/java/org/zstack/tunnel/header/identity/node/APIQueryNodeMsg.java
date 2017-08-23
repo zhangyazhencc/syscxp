50d19c2afbe7ec2cc5caa267bb0a4fd28b47bcdb
dingchunyu <dingchunyu@syscloud.cn>
2017-08-23 15:48:47 +0800

    update

--- tunnel/src/main/java/org/zstack/tunnel/header/identity/node/APIQueryNodeMsg.java
+++ tunnel/src/main/java/org/zstack/tunnel/header/identity/node/APIQueryNodeMsg.java
@@ -6,6 +6,6 @@ import org.zstack.header.query.AutoQuery;
 /**
  * Created by DCY on 2017-08-21
  */
-@AutoQuery(replyClass = ApiQueryNodeReply.class, inventoryClass = NodeInventory.class)
-public class ApiQueryNodeMsg extends APIQueryMessage {
+@AutoQuery(replyClass = APIQueryNodeReply.class, inventoryClass = NodeInventory.class)
+public class APIQueryNodeMsg extends APIQueryMessage {
 }
