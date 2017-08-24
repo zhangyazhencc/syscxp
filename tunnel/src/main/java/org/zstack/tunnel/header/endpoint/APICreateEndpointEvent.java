d02a746ad2c919f2eac015686282f281be325fae
dingchunyu <dingchunyu@syscloud.cn>
2017-08-24 13:08:57 +0800

    update

--- tunnel/src/main/java/org/zstack/tunnel/header/endpoint/APICreateEndpointEvent.java
+++ tunnel/src/main/java/org/zstack/tunnel/header/endpoint/APICreateEndpointEvent.java
@@ -1,7 +1,24 @@
 package org.zstack.tunnel.header.endpoint;
 
+import org.zstack.header.message.APIEvent;
+import org.zstack.header.rest.RestResponse;
+
 /**
  * Created by DCY on 2017-08-23
  */
-public class APICreateEndpointEvent {
+@RestResponse(allTo = "inventory")
+public class APICreateEndpointEvent extends APIEvent {
+    private EndpointInventory inventory;
+
+    public APICreateEndpointEvent(){}
+
+    public APICreateEndpointEvent(String apiId){super(apiId);}
+
+    public EndpointInventory getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(EndpointInventory inventory) {
+        this.inventory = inventory;
+    }
 }
--- tunnel/src/main/java/org/zstack/tunnel/header/endpoint/APIUpdateEndpointMsg.java
+++ tunnel/src/main/java/org/zstack/tunnel/header/endpoint/APIUpdateEndpointMsg.java
@@ -1,7 +1,42 @@
 package org.zstack.tunnel.header.endpoint;
 
+import org.zstack.header.message.APIMessage;
+import org.zstack.header.message.APIParam;
+
 /**
  * Created by DCY on 2017-08-23
  */
-public class APIUpdateEndpointMsg {
+public class APIUpdateEndpointMsg extends APIMessage {
+    @APIParam(nonempty = true,resourceType = EndpointVO.class)
+    private String targetUuid;
+
+    @APIParam(required = false,maxLength = 255)
+    private String name;
+
+    @APIParam(required = false,maxLength = 128)
+    private String code;
+
+    public String getTargetUuid() {
+        return targetUuid;
+    }
+
+    public void setTargetUuid(String targetUuid) {
+        this.targetUuid = targetUuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 }
