ed8f69296f0fc5b6fe67b6aa3dd81e0575a0c28f
caozhenlu <caozhenlu@syscloud.cn>
2017-09-06 08:51:27 +0800

    fixed the code

--- billing/src/main/java/org/zstack/billing/header/bill/APIQueryBillMsg.java
+++ billing/src/main/java/org/zstack/billing/header/bill/APIQueryBillMsg.java
@@ -1,5 +1,6 @@
 package org.zstack.billing.header.bill;
 
+import org.zstack.billing.manage.APIQueryExpendMessage;
 import org.zstack.billing.manage.BillingConstant;
 import org.zstack.header.identity.Action;
 import org.zstack.header.message.APIParam;
@@ -8,16 +9,6 @@ import org.zstack.header.query.AutoQuery;
 
 @Action(category = BillingConstant.ACTION_CATEGORY, names = {"read", "bill"})
 @AutoQuery(replyClass = APIQueryBillReply.class, inventoryClass = BillInventory.class)
-public class APIQueryBillMsg extends APIQueryMessage {
+public class APIQueryBillMsg extends APIQueryExpendMessage {
 
-    @APIParam
-    private boolean selfSelect;
-
-    public boolean isSelfSelect() {
-        return selfSelect;
-    }
-
-    public void setSelfSelect(boolean selfSelect) {
-        this.selfSelect = selfSelect;
-    }
 }
