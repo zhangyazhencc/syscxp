64425279284db2f59da52eeffc20348ca103f10c
caozhenlu <caozhenlu@syscloud.cn>
2017-09-11 16:08:54 +0800

    fix a bug

--- billing/src/main/java/org/zstack/billing/header/balance/APIQueryDisChargeMsg.java
+++ billing/src/main/java/org/zstack/billing/header/balance/APIQueryDisChargeMsg.java
@@ -3,7 +3,9 @@ package org.zstack.billing.header.balance;
 import org.zstack.billing.manage.BillingConstant;
 import org.zstack.header.identity.Action;
 import org.zstack.header.query.APIQueryMessage;
+import org.zstack.header.query.AutoQuery;
 
 @Action(category = BillingConstant.ACTION_CATEGORY, names = {"recharge", "read"})
+@AutoQuery(replyClass = APIQueryDisChargeReply.class, inventoryClass = AccountDischargeInventory.class)
 public class APIQueryDisChargeMsg  extends APIQueryMessage {
 }
