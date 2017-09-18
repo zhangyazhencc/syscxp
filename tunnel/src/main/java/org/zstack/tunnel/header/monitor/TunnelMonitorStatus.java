99dad1a0ca20b8e3b7835b15c84e3edeb0ceab68
sunxuelong <sunxuelong@syscloud.cn>
2017-09-18 14:15:52 +0800

    add speed records

--- tunnel/src/main/java/org/zstack/tunnel/header/monitor/TunnelMonitorStatus.java
+++ tunnel/src/main/java/org/zstack/tunnel/header/monitor/TunnelMonitorStatus.java
@@ -3,7 +3,7 @@ package org.zstack.tunnel.header.monitor;
 /**
  * @Author: sunxuelong.
  * @Cretion Date: 2017-09-14.
- * @Description: .
+ * @Description: 通道监控状态.
  */
 public enum TunnelMonitorStatus {
     NORMAL,
