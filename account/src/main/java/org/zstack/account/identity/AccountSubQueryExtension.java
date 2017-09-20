e80204be3753845ed29635363496cffd8f24cfb5
wangwg <wangwg@syscloud.cn>
2017-09-20 17:20:07 +0800

    fix

--- account/src/main/java/org/zstack/account/identity/AccountSubQueryExtension.java
+++ account/src/main/java/org/zstack/account/identity/AccountSubQueryExtension.java
@@ -1,15 +1,28 @@
 package org.zstack.account.identity;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.zstack.account.header.account.AccountConstant;
+import org.zstack.account.header.account.AccountVO;
+import org.zstack.account.header.account.AccountVO_;
+import org.zstack.account.header.identity.ProxyAccountRefVO;
+import org.zstack.account.header.identity.ProxyAccountRefVO_;
+import org.zstack.core.db.DatabaseFacade;
+import org.zstack.core.db.SimpleQuery;
+import org.zstack.header.identity.AccountType;
 import org.zstack.header.query.APIQueryMessage;
 import org.zstack.query.AbstractMysqlQuerySubQueryExtension;
 import org.zstack.query.QueryUtils;
+
+import java.util.List;
 
 /**
  */
 public class AccountSubQueryExtension extends AbstractMysqlQuerySubQueryExtension {
     @Autowired
     private IdentiyInterceptor identiyInterceptor;
+
+    @Autowired
+    private DatabaseFacade dbf;
 
     @Override
     public String makeSubquery(APIQueryMessage msg, Class inventoryClass) {
@@ -22,7 +35,25 @@ public class AccountSubQueryExtension extends AbstractMysqlQuerySubQueryExtensio
             return null;
         }
 
-        //String priKey = QueryUtils.getPrimaryKeyNameFromEntityClass(entityClass);
+        //String priKey = QueryUtils.getPrimaryKeyNameFromEntityClass(entityClass);\
+
+        if(msg.getSession().getType() == AccountType.Proxy){
+            StringBuffer uuidlist = new StringBuffer() ;
+            uuidlist.append("");
+            SimpleQuery<ProxyAccountRefVO> q = dbf.createQuery(ProxyAccountRefVO.class);
+            q.add(ProxyAccountRefVO_.accountUuid, SimpleQuery.Op.EQ, msg.getSession().getAccountUuid());
+            if(q.list() != null && q.list().size() > 0){
+                for(Object pr : q.list()){
+                    ProxyAccountRefVO ps = (ProxyAccountRefVO)pr;
+                    uuidlist.append("'" + ps.getCustomerAcccountUuid().toString() + "',");
+                }
+            }
+            if(uuidlist.length() > 0){
+                uuidlist.substring(0,uuidlist.length());
+            }
+            return String.format("%s.uuid in (%s)", inventoryClass.getSimpleName().toLowerCase()
+                    , uuidlist) ;
+        }
 
         return String.format("%s.accountUuid = '%s'", inventoryClass.getSimpleName().toLowerCase()
                 , msg.getSession().getAccountUuid()) ;
