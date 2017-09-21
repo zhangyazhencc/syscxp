de65074c0cdcaf127e9826179cb6a1deaef629a7
wangwg <wangwg@syscloud.cn>
2017-09-21 14:32:58 +0800

    add

---
+++ account/src/main/java/org/zstack/account/header/account/APIDeleteProxyAccountRefEvent.java
@@ -0,0 +1,18 @@
+package org.zstack.account.header.account;
+
+import org.zstack.header.message.APIEvent;
+
+/**
+ * Created by wangwg on 2017/09/21.
+ */
+
+public class APIDeleteProxyAccountRefEvent extends APIEvent {
+    public APIDeleteProxyAccountRefEvent() {
+    }
+
+    public APIDeleteProxyAccountRefEvent(String apiId) {
+        super(apiId);
+    }
+ 
+
+}
