21d62f4c637392029bb337ed4fa7eb18cdada3f5
wangjie <wangjie@syscloud.cn>
2017-09-21 11:08:18 +0800

    fix

--- account/src/main/java/org/zstack/account/header/account/APIGetAccountMsg.java
+++ account/src/main/java/org/zstack/account/header/account/APIGetAccountMsg.java
@@ -1,8 +1,10 @@
 package org.zstack.account.header.account;
 
 import org.zstack.header.identity.Action;
+import org.zstack.header.identity.UserCredentialCheck;
 import org.zstack.header.message.APISyncCallMessage;
 
+@UserCredentialCheck
 @Action(category = AccountConstant.ACTION_CATEGORY_ACCOUNT, names = {"read"})
 public class APIGetAccountMsg extends APISyncCallMessage implements  AccountMessage {
 
