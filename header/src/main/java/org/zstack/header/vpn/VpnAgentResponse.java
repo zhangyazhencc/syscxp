af7697517f9b3bbd050806f3ebf5b7678f481a29
wangjie <wangjie@syscloud.cn>
2017-09-30 14:10:04 +0800

    update vpn response

--- header/src/main/java/org/zstack/header/vpn/VpnAgentResponse.java
+++ header/src/main/java/org/zstack/header/vpn/VpnAgentResponse.java
@@ -8,6 +8,24 @@ import org.zstack.header.rest.RestAPIState;
  */
 public class VpnAgentResponse {
     private HttpStatus statusCode;
+    private RestAPIState state;
+    private TaskResult result;
+
+    public TaskResult getResult() {
+        return result;
+    }
+
+    public void setResult(TaskResult result) {
+        this.result = result;
+    }
+
+    public RestAPIState getState() {
+        return state;
+    }
+
+    public void setState(RestAPIState state) {
+        this.state = state;
+    }
 
     public HttpStatus getStatusCode() {
         return statusCode;
@@ -15,6 +33,27 @@ public class VpnAgentResponse {
 
     public void setStatusCode(HttpStatus statusCode) {
         this.statusCode = statusCode;
+    }
+
+    public static class TaskResult {
+        private String message;
+        private boolean success = true;
+
+        public String getError() {
+            return message;
+        }
+
+        public void setError(String message) {
+            this.message = message;
+        }
+
+        public boolean isSuccess() {
+            return success;
+        }
+
+        public void setSuccess(boolean success) {
+            this.success = success;
+        }
     }
 
 }
