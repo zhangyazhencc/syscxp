c4d773dde906e8f56fff16ed996caa6c97710dd4
wangwg <wangwg@syscloud.cn>
2017-10-12 15:56:30 +0800

    update

--- account/src/main/java/com/syscxp/account/header/identity/APIVerifyRepetitionReply.java
+++ account/src/main/java/com/syscxp/account/header/identity/APIVerifyRepetitionReply.java
@@ -1,5 +1,6 @@
 package com.syscxp.account.header.identity;
 
+import com.syscxp.header.message.APIParam;
 import com.syscxp.header.message.APIReply;
 
 /**
@@ -11,6 +12,9 @@ public class APIVerifyRepetitionReply extends APIReply {
     private boolean accountEmail;
     private boolean accountPhone;
     private boolean userName;
+    private boolean userEmail;
+    private boolean userPhone;
+    private boolean roleName;
 
     public boolean isAccountName() {
         return accountName;
@@ -26,6 +30,30 @@ public class APIVerifyRepetitionReply extends APIReply {
 
     public void setAccountEmail(boolean accountEmail) {
         this.accountEmail = accountEmail;
+    }
+
+    public boolean isUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(boolean userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public boolean isUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(boolean userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public boolean isRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(boolean roleName) {
+        this.roleName = roleName;
     }
 
     public boolean isAccountPhone() {
