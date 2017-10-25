1fa701333ffd70cadb717eaae1ec7074e271a254
wangwg <wangwg@syscloud.cn>
2017-10-25 15:42:48 +0800

    update

--- tunnel/src/main/java/com/syscxp/tunnel/manage/ImageUploadInfoConstant.java
+++ tunnel/src/main/java/com/syscxp/tunnel/manage/ImageUploadInfoConstant.java
@@ -8,9 +8,9 @@ import com.syscxp.header.configuration.PythonClass;
 @PythonClass
 public interface ImageUploadInfoConstant {
 
-    String upload_url = "http://172.16.123.129/node_images/node_images_upload.php";
-    String delete_url = "http://172.16.123.129/node_images/node_images_delete.php";
-    String image_url_prefix = "http://172.16.123.129/node_images/";
+    String upload_url = "http://192.168.211.5/node_images/node_images_upload.php";
+    String delete_url = "http://192.168.211.5/node_images/node_images_delete.php";
+    String image_url_prefix = "http://192.168.211.5/node_images/";
     String upload_key = "sysclouduploadkey";
     String fileNumLimit = "5";
 
