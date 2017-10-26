6981cc31a6d690fd063e1d548f44ea842a730c17
dingchunyu <dingchunyu@syscloud.cn>
2017-10-26 08:54:47 +0800

    UPDATE

---
+++ tunnel/src/main/java/com/syscxp/tunnel/header/tunnel/TaskStatus.java
@@ -0,0 +1,10 @@
+package com.syscxp.tunnel.header.tunnel;
+
+/**
+ * Create by DCY on 2017/10/25
+ */
+public enum TaskStatus {
+    Preexecute,
+    Success,
+    Fail
+}
--- tunnel/src/main/java/com/syscxp/tunnel/header/tunnel/TunnelAO_.java
+++ tunnel/src/main/java/com/syscxp/tunnel/header/tunnel/TunnelAO_.java
@@ -28,6 +28,7 @@ public class TunnelAO_ {
     public static volatile SingularAttribute<TunnelAO, Integer> duration;
     public static volatile SingularAttribute<TunnelAO, ProductChargeModel> productChargeModel;
     public static volatile SingularAttribute<TunnelAO, Integer> maxModifies;
+    public static volatile SingularAttribute<TunnelAO, Integer> isBilling;
     public static volatile SingularAttribute<TunnelAO, String> description;
     public static volatile SingularAttribute<TunnelAO, Timestamp> expiredDate;
     public static volatile SingularAttribute<TunnelAO, Timestamp> lastOpDate;
