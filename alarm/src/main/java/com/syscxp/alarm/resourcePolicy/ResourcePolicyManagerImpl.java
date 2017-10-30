6e814e5b78c3833a7d9b60a39d5490794504a307
wangwg <wangwg@syscloud.cn>
2017-10-30 18:42:59 +0800

    update

--- alarm/src/main/java/com/syscxp/alarm/resourcePolicy/ResourcePolicyManagerImpl.java
+++ alarm/src/main/java/com/syscxp/alarm/resourcePolicy/ResourcePolicyManagerImpl.java
@@ -394,6 +394,13 @@ public class ResourcePolicyManagerImpl  extends AbstractService implements ApiMe
         regulationVO.setTriggerPeriod(msg.getTriggerPeriod());
         dbf.persistAndRefresh(regulationVO);
 
+        SimpleQuery<ResourcePolicyRefVO> rpr = dbf.createQuery(ResourcePolicyRefVO.class);
+        rpr.add(ResourcePolicyRefVO_.policyUuid, SimpleQuery.Op.EQ, msg.getPolicyUuid());
+        if(rpr.isExists()){
+            List<ResourcePolicyRefVO> list = rpr.list();
+            
+        }
+
         APICreateRegulationEvent event = new APICreateRegulationEvent(msg.getId());
         event.setInventory(RegulationInventory.valueOf(regulationVO));
         bus.publish(event);
