5325e85278bdc25f34352bf75f19b80f28ddfa6a
zhangqiuyu <zhangqiuyu@syscloud.cn>
2017-10-31 15:49:33 +0800

    modify

--- alarm/src/main/java/com/syscxp/alarm/log/ALARMLOGManagerImpl.java
+++ alarm/src/main/java/com/syscxp/alarm/log/ALARMLOGManagerImpl.java
@@ -1,9 +1,8 @@
 package com.syscxp.alarm.log;
 
+import com.cloopen.rest.sdk.CCPRestSDK;
 import com.syscxp.alarm.header.contact.*;
-import com.syscxp.alarm.header.log.APICreateAlarmLogMsg;
-import com.syscxp.alarm.header.log.AlarmLogVO;
-import com.syscxp.alarm.header.log.AlarmStatus;
+import com.syscxp.alarm.header.log.*;
 import com.syscxp.core.Platform;
 import com.syscxp.core.cloudbus.CloudBus;
 import com.syscxp.core.db.DatabaseFacade;
@@ -15,19 +14,24 @@ import com.syscxp.header.alarm.AlarmConstant;
 import com.syscxp.header.apimediator.ApiMessageInterceptionException;
 import com.syscxp.header.apimediator.ApiMessageInterceptor;
 import com.syscxp.header.billing.ProductType;
+import com.syscxp.header.errorcode.OperationFailureException;
+import com.syscxp.header.identity.SessionInventory;
 import com.syscxp.header.message.APIMessage;
 import com.syscxp.header.message.Message;
 import com.syscxp.header.rest.RESTFacade;
+import com.syscxp.sms.MailService;
+import com.syscxp.sms.SmsGlobalProperty;
+import com.syscxp.sms.SmsService;
+import com.syscxp.sms.SmsServiceImpl;
+import com.syscxp.sms.header.SmsVO;
 import com.syscxp.utils.Utils;
 import com.syscxp.utils.logging.CLogger;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.persistence.Tuple;
-import javax.persistence.metamodel.SingularAttribute;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
+import java.sql.Timestamp;
+import java.util.*;
+
+import static com.syscxp.core.Platform.operr;
 
 public class ALARMLOGManagerImpl  extends AbstractService implements ApiMessageInterceptor {
 
@@ -43,6 +47,10 @@ public class ALARMLOGManagerImpl  extends AbstractService implements ApiMessageI
     private ErrorFacade errf;
     @Autowired
     private RESTFacade restf;
+    @Autowired
+    private SmsService smsService;
+    @Autowired
+    private MailService mailService;
 
     @Override
     public void handleMessage(Message msg) {
@@ -62,6 +70,7 @@ public class ALARMLOGManagerImpl  extends AbstractService implements ApiMessageI
     }
 
     private void handle(APICreateAlarmLogMsg msg) {
+
         AlarmLogVO alarmLogVO = new AlarmLogVO();
         alarmLogVO.setUuid(Platform.getUuid());
         alarmLogVO.setProductUuid(msg.getTunnel_id());
@@ -69,24 +78,90 @@ public class ALARMLOGManagerImpl  extends AbstractService implements ApiMessageI
         alarmLogVO.setAlarmContent(msg.getProblem());
         alarmLogVO.setResumeTime(msg.getResumed());
         alarmLogVO.setAlarmTime(msg.getCreated());
-        alarmLogVO.setStatus(AlarmStatus.nameOf(msg.getStatus()));
+        alarmLogVO.setStatus(msg.getStatus());
         alarmLogVO.setAccountUuid(msg.getUser_id());
         alarmLogVO.setProductType(ProductType.TUNNEL);//todo  where should acquire
+        SimpleQuery<AlarmLogVO> queryTime = dbf.createQuery(AlarmLogVO.class);
+        queryTime.add(AlarmLogVO_.productUuid, SimpleQuery.Op.EQ, msg.getTunnel_id());
+        queryTime.orderBy(AlarmLogVO_.createDate, SimpleQuery.Od.DESC);
+        List<AlarmLogVO> alarmLogList = queryTime.list();
+        int count = 0;
+        int countSize = alarmLogList.size();
+        Timestamp time = null;
+        for(AlarmLogVO vo: alarmLogList){
+          if(msg.getStatus().equals(AlarmStatus.RESUME)){
+              if(vo.getStatus().equals(AlarmStatus.RESUME)){
+                  if(time != null){
+                      long times = time.getTime();
+                      long currentTime = System.currentTimeMillis();
+                      long diffTime = currentTime-times;
+                      alarmLogVO.setDuration((int)diffTime/1000);
+                      break;
+                  }
+
+              }
+          }else if(msg.getStatus().equals(AlarmStatus.ALARM)){
+              if(vo.getStatus().equals(AlarmStatus.RESUME)){
+                  if(time != null){
+                      long times = time.getTime();
+                      long currentTime = System.currentTimeMillis();
+                      long diffTime = currentTime-times;
+                      alarmLogVO.setDuration((int)diffTime/1000);
+                      break;
+                  }
+              }
+              if(count+1 == countSize-1){
+                  long diffTime = System.currentTimeMillis() - vo.getCreateDate().getTime();
+                  alarmLogVO.setDuration((int)diffTime/1000);
+              }
+          }
+          time = vo.getCreateDate();
+          count++;
+        }
+
         //alarmLogVO.setDuration(0);//持续时间
         //alarmLogVO.setDurationTimeUnit(TimeUnit.MILLISECONDS);
         dbf.persistAndRefresh(alarmLogVO);
         SimpleQuery<ContactGroupVO> query = dbf.createQuery(ContactGroupVO.class);
         query.add(ContactGroupVO_.accountUuid, SimpleQuery.Op.EQ,msg.getUser_id());
         List<ContactGroupVO> groups = query.list();
-        for(ContactGroupVO contactGroupVO : groups){
+         for(ContactGroupVO contactGroupVO : groups){
             for(ContactVO contactVO: contactGroupVO.getContactVOList()){
                 Set<NotifyWayVO> notifyWayVOs = contactVO.getNotifyWayVOs();
                 for(NotifyWayVO notifyWayVO: notifyWayVOs){
-                    notifyWayVO.getCode().equals("email");
+                    if( notifyWayVO.getCode().equals("email")){
+                        String email = contactVO.getEmail();
+                        try{
+                            Boolean result = mailService.mailSend(email,"监控报警信息",msg.getProblem());
+                        }catch (Exception e){
+                            //保存失败信息
+                            e.printStackTrace();
+                            throw new OperationFailureException(operr("message:" + e.getMessage()));
+                        }
+                        logger.debug("发送告警的内容是：" + msg.getProblem() +
+                                "[邮件:"+email+"]");
+
+                    }
+
+                    if( notifyWayVO.getCode().equals("phone")){
+                        String phone = contactVO.getMobile();
+                        SmsVO sms = smsService.sendMsg(msg.getSession(), phone, SmsGlobalProperty.SMS_VERIFICATION_CODE_APPID, SmsGlobalProperty.SMS_VERIFICATION_CODE_AlARM
+                                , new String[]{msg.getProblem(), "10"}, msg.getIp());
+//                        if(sms.getStatusCode() != "000000"){
+//                            //保存失败信息
+//
+//                        }
+                        logger.debug("发送告警的内容是：" + msg.getProblem() +
+                                "[手机号码:"+phone+"]");
+                    }
+
                 }
             }
         }
         //foreach发送短信和邮件
+        APICreateALarmLogEvent evt = new APICreateALarmLogEvent(msg.getId());
+        evt.setInventory(AlarmLogInventory.valueOf(alarmLogVO));
+        bus.publish(evt);
 
 
     }
@@ -114,4 +189,8 @@ public class ALARMLOGManagerImpl  extends AbstractService implements ApiMessageI
     public APIMessage intercept(APIMessage msg) throws ApiMessageInterceptionException {
         return msg;
     }
+
+
+
+
 }
