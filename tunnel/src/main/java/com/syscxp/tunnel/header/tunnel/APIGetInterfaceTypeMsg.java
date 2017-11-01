6de318b96e5daf88030289a303fe6e99220d6fe2
wangjie <717728534@qq.com>
2017-11-01 15:17:50 +0800

    update get type

---
+++ tunnel/src/main/java/com/syscxp/tunnel/header/tunnel/APIGetInterfaceTypeMsg.java
@@ -0,0 +1,24 @@
+package com.syscxp.tunnel.header.tunnel;
+
+import com.syscxp.header.identity.Action;
+import com.syscxp.header.message.APIParam;
+import com.syscxp.header.message.APISyncCallMessage;
+import com.syscxp.header.tunnel.TunnelConstant;
+import com.syscxp.tunnel.header.endpoint.EndpointVO;
+
+/**
+ * Created by DCY on 2017-09-11
+ */
+@Action(category = TunnelConstant.ACTION_CATEGORY, names = {"read"})
+public class APIGetInterfaceTypeMsg extends APISyncCallMessage {
+    @APIParam(emptyString = false,resourceType = EndpointVO.class)
+    private String uuid;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+}
