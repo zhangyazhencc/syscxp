3805525e6143a96cfe420c9b448bf0cb8f8a59f9
zhangqiuyu <zhangqiuyu@syscloud.cn>
2017-11-02 17:42:09 +0800

    策略的修改

--- tunnel/src/main/java/com/syscxp/tunnel/header/node/APIListCountryNodeMsg.java
+++ tunnel/src/main/java/com/syscxp/tunnel/header/node/APIListCountryNodeMsg.java
@@ -1,8 +1,10 @@
 package com.syscxp.tunnel.header.node;
 
+import com.syscxp.header.identity.Action;
 import com.syscxp.header.message.APISyncCallMessage;
+import com.syscxp.tunnel.manage.NodeConstant;
 
-
+@Action(category = NodeConstant.ACTION_CATEGORY, names = {"read"})
 public class APIListCountryNodeMsg extends APISyncCallMessage{
 
 }
