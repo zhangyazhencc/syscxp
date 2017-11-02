4f7db32d8f309530fa9f262b64739f79c1ce5d88
wangwg <wangwg@syscloud.cn>
2017-11-02 14:00:52 +0800

    fix

--- alarm/src/main/java/com/syscxp/alarm/AlarmGlobalProperty.java
+++ alarm/src/main/java/com/syscxp/alarm/AlarmGlobalProperty.java
@@ -9,7 +9,7 @@ public class AlarmGlobalProperty {
     @GlobalProperty(name = "tunnelServerUrl",defaultValue = "http://192.168.211.99/tunnel/api")
     public static String TUNNEL_SERVER_RUL;
 
-    @GlobalProperty(name = "falconUrl_save",defaultValue = "http://192.168.211.100:6892/monitoring/strategy/save")
+    @GlobalProperty(name = "falconUrlSave",defaultValue = "http://192.168.211.100:6892/monitoring/strategy/save")
     public static String FALCON_URL_SAVE;
 
     @GlobalProperty(name = "falconUrlDelete",defaultValue = "http://192.168.211.100:6892/monitoring/strategy/delete")
---
+++ header/src/main/java/com/syscxp/header/alarm/APIUpdateTunnelInfoForFalconReply.java
@@ -0,0 +1,12 @@
+package com.syscxp.header.alarm;
+
+import com.syscxp.header.query.APIQueryReply;
+
+import java.util.Map;
+
+/**
+ * Created by wangwg on 2017/11/02
+ */
+public class APIUpdateTunnelInfoForFalconReply extends APIQueryReply {
+
+}
