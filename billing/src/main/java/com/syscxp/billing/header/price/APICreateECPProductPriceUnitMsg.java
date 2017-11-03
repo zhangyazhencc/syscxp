9922857326a746c3c4a0f76c280853e7a0e9edda
zhangqiuyu <zhangqiuyu@syscloud.cn>
2017-11-03 17:45:23 +0800

    modify

--- billing/src/main/java/com/syscxp/billing/header/price/APICreateECPProductPriceUnitMsg.java
+++ billing/src/main/java/com/syscxp/billing/header/price/APICreateECPProductPriceUnitMsg.java
@@ -7,21 +7,47 @@ import com.syscxp.header.message.APIMessage;
 import com.syscxp.header.message.APIParam;
 
 @Action(category = BillingConstant.ACTION_CATEGORY_PRICE)
-public class APICreateVHostProductPriceUnitMsg extends APIMessage{
+public class APICreateECPProductPriceUnitMsg extends APIMessage{
     @APIParam(maxLength = 256,emptyString = false)
     private String productCategoryUuid;
+
+    @APIParam( maxLength = 256,emptyString = false)
+    private String areaCode;
 
     @APIParam( maxLength = 256,emptyString = false)
     private String areaName;
 
     @APIParam( maxLength = 256,emptyString = false)
+    private String lineCode;
+
+    @APIParam( maxLength = 256,emptyString = false)
     private String lineName;
+
+    @APIParam(maxLength = 256,emptyString = false)
+    private String configCode;
 
     @APIParam(maxLength = 256,emptyString = false)
     private String configName;
 
     @APIParam(maxLength = 256,emptyString = false)
     private Integer unitPrice;
+
+    public String getAreaCode() {
+        return areaCode;
+    }
+
+    public void setAreaCode(String areaCode) {
+        this.areaCode = areaCode;
+    }
+
+
+    public String getLineCode() {
+        return lineCode;
+    }
+
+    public void setLineCode(String lineCode) {
+        this.lineCode = lineCode;
+    }
 
     public String getProductCategoryUuid() {
         return productCategoryUuid;
@@ -61,5 +87,13 @@ public class APICreateVHostProductPriceUnitMsg extends APIMessage{
 
     public void setUnitPrice(Integer unitPrice) {
         this.unitPrice = unitPrice;
+    }
+
+    public String getConfigCode() {
+        return configCode;
+    }
+
+    public void setConfigCode(String configCode) {
+        this.configCode = configCode;
     }
 }
