58f079cbf60e18a0b17458d904f41d2cf08aa530
zlcao <caozhenlu@syscloud.cn>
2017-11-14 17:03:56 +0800

    fix a bug

--- header/src/main/java/com/syscxp/header/account/APIValidateAccountReply.java
+++ header/src/main/java/com/syscxp/header/account/APIValidateAccountReply.java
@@ -2,10 +2,22 @@ package com.syscxp.header.account;
 
 import com.syscxp.header.message.APIReply;
 
+import java.util.List;
+
 public class APIValidateAccountReply extends APIReply {
 
     private boolean validAccount;
     private boolean normalAccountHasProxy;
+
+    private List<String> accountUuidOwnProxy;
+
+    public List<String> getAccountUuidOwnProxy() {
+        return accountUuidOwnProxy;
+    }
+
+    public void setAccountUuidOwnProxy(List<String> accountUuidOwnProxy) {
+        this.accountUuidOwnProxy = accountUuidOwnProxy;
+    }
 
     public boolean isValidAccount() {
         return validAccount;
