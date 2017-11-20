2b80d29df8707a7f7a5790af4b2cb4587ccb6290
zhangqiuyu <zhangqiuyu@syscloud.cn>
2017-11-20 09:42:49 +0800

    modify

--- header/src/main/java/com/syscxp/header/tunnel/aliEdgeRouter/APIQueryAliEdgeRouterMsg.java
+++ header/src/main/java/com/syscxp/header/tunnel/aliEdgeRouter/APIQueryAliEdgeRouterMsg.java
@@ -1,8 +1,11 @@
 package com.syscxp.header.tunnel.aliEdgeRouter;
 
+import com.syscxp.header.identity.Action;
 import com.syscxp.header.query.APIQueryMessage;
 import com.syscxp.header.query.AutoQuery;
+import com.syscxp.header.tunnel.TunnelConstant;
 
+@Action(category = TunnelConstant.ACTION_CATEGORY, names = {"read"})
 @AutoQuery(replyClass = APIQueryAliEdgeRouterReply.class, inventoryClass = AliEdgeRouterInventory.class)
 public class APIQueryAliEdgeRouterMsg extends APIQueryMessage{
 }
