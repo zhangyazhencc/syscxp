f4524327e968142ebf29d87542c4a3618f8ec06e
wangwg <wangwg@syscloud.cn>
2017-11-21 13:33:02 +0800

    update

---
+++ header/src/main/java/com/syscxp/header/tunnel/solution/APIQuerySolutionMsg.java
@@ -0,0 +1,17 @@
+package com.syscxp.header.tunnel.solution;
+
+import com.syscxp.header.identity.Action;
+import com.syscxp.header.query.APIQueryMessage;
+import com.syscxp.header.query.AutoQuery;
+import com.syscxp.header.tunnel.NodeConstant;
+import com.syscxp.header.tunnel.node.APIQueryNodeReply;
+import com.syscxp.header.tunnel.node.NodeInventory;
+
+/**
+ * Created by wangwg on 2017/11/21
+ */
+
+@Action(category = SolutionConstant.ACTION_CATEGORY, names = {"read"})
+@AutoQuery(replyClass = APIQuerySolutionReply.class, inventoryClass = SolutionInventory.class)
+public class APIQuerySolutionMsg extends APIQueryMessage {
+}
--- tunnel/src/main/java/com/syscxp/tunnel/solution/SolutionManagerImpl.java
+++ tunnel/src/main/java/com/syscxp/tunnel/solution/SolutionManagerImpl.java
@@ -1,5 +1,6 @@
 package com.syscxp.tunnel.solution;
 
+import com.syscxp.core.Platform;
 import com.syscxp.core.cloudbus.CloudBus;
 import com.syscxp.core.db.DatabaseFacade;
 import com.syscxp.core.errorcode.ErrorFacade;
@@ -9,7 +10,7 @@ import com.syscxp.header.apimediator.ApiMessageInterceptor;
 import com.syscxp.header.message.APIMessage;
 import com.syscxp.header.message.Message;
 import com.syscxp.header.rest.RESTFacade;
-import com.syscxp.header.tunnel.solution.SolutionConstant;
+import com.syscxp.header.tunnel.solution.*;
 import com.syscxp.tunnel.tunnel.TunnelBase;
 import com.syscxp.tunnel.tunnel.TunnelManagerImpl;
 import com.syscxp.utils.Utils;
@@ -49,11 +50,51 @@ public class SolutionManagerImpl extends AbstractService implements SolutionMana
     }
 
     private void handleApiMessage(APIMessage msg) {
-        if(false){
-
-        } else {
+        if(msg instanceof APICreateSolutionMsg){
+            hand((APICreateSolutionMsg) msg);
+        } else if(msg instanceof APICreateSolutionInterfaceMsg){
+            hand((APICreateSolutionInterfaceMsg) msg);
+        }else if(msg instanceof APICreateSolutionTunnelMsg){
+            hand((APICreateSolutionTunnelMsg) msg);
+        }else if(msg instanceof APICreateSolutionVpnMsg){
+            hand((APICreateSolutionVpnMsg) msg);
+        }else {
             bus.dealWithUnknownMessage(msg);
         }
+    }
+
+    private void hand(APICreateSolutionInterfaceMsg msg) {
+        SolutionInterfaceVO vo = new SolutionInterfaceVO();
+        vo.setUuid(Platform.getUuid());
+        vo.setCost(msg.getCost());
+        vo.setDuration(msg.getDuration());
+        vo.setProductChargeModel(msg.getProductChargeModel());
+        vo.setSolutionUuid(msg.getSolutionUuid());
+        if(msg.getDescription() != null){
+            vo.setDescription(msg.getDescription());
+        }
+        if(msg.getName() != null){
+            vo.setName(msg.getName());
+        }
+
+        vo.setEndpointName(msg.getEndpointName());
+        vo.setPortOfferingName(msg.getPortOfferingName());
+
+        APICreateSolutionInterfaceEvent event = new APICreateSolutionInterfaceEvent(msg.getId());
+        event.setInventory(SolutionInterfaceInventory.valueOf(dbf.persistAndRefresh(vo)));
+        bus.publish(event);
+
+    }
+
+    private void hand(APICreateSolutionMsg msg) {
+        SolutionVO vo = new SolutionVO();
+        vo.setUuid(Platform.getUuid());
+        vo.setName(msg.getName());
+        vo.setDescription(msg.getDescription());
+
+        APICreateSolutionEvent event = new APICreateSolutionEvent(msg.getId());
+        event.setInventory(SolutionInventory.valueOf(dbf.persistAndRefresh(vo)));
+        bus.publish(event);
     }
 
 
