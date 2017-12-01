7e606963a0ca7762c2dc579463f003bd97593d36
wangwg <wangwg@syscloud.cn>
2017-12-01 17:46:01 +0800

    update

--- sms/src/main/java/com/syscxp/sms/ImageCodeService.java
+++ sms/src/main/java/com/syscxp/sms/ImageCodeService.java
@@ -1,6 +1,9 @@
 package com.syscxp.sms;
 
+/**
+ * Created by wangwg on 2017/12/01.
+ */
 
 public interface ImageCodeService {
-    boolean ValidateImageCode(String mail, String code);
+    boolean ValidateImageCode(String imageId, String code);
 }
---
+++ sms/src/main/java/com/syscxp/sms/header/APIGetImageCodeMsg.java
@@ -0,0 +1,12 @@
+package com.syscxp.sms.header;
+
+import com.syscxp.header.identity.SuppressCredentialCheck;
+import com.syscxp.header.message.APIParam;
+import com.syscxp.header.message.APISyncCallMessage;
+
+/**
+ * Created by wangwg on 2017/12/01.
+ */
+@SuppressCredentialCheck
+public class APIGetImageCodeMsg extends APISyncCallMessage {
+}
