4f923024029aa4a89a6089f11894deb3395d4abf
wangwg <wangwg@syscloud.cn>
2017-12-04 14:57:53 +0800

    update

--- sms/src/main/java/com/syscxp/sms/Test.java
+++
@@ -1,8 +0,0 @@
-package com.syscxp.sms;
-
-public class Test {
-    public static void main(String[] args) {
-        ImageCodeServiceImpl image = new ImageCodeServiceImpl();
-
-    }
-}
--- sms/src/main/java/com/syscxp/sms/header/APIGetImageCodeReply.java
+++ sms/src/main/java/com/syscxp/sms/header/APIGetImageCodeReply.java
@@ -6,5 +6,24 @@ import com.syscxp.header.message.APIReply;
  * Created by wangwg on 2017/12/01.
  */
 public class APIGetImageCodeReply extends APIReply {
-    
+
+    String ImageUuid;
+
+    String ImageCode;
+
+    public String getImageUuid() {
+        return ImageUuid;
+    }
+
+    public void setImageUuid(String imageUuid) {
+        ImageUuid = imageUuid;
+    }
+
+    public String getImageCode() {
+        return ImageCode;
+    }
+
+    public void setImageCode(String imageCode) {
+        ImageCode = imageCode;
+    }
 }
