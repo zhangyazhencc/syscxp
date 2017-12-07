0bcc30a6d6e0d63b8b1e23cd6bfe1b4d4a8ad3b6
wangjie <717728534@qq.com>
2017-12-07 14:04:48 +0800

    update

--- vpn/src/main/java/com/syscxp/vpn/host/VpnHost.java
+++ vpn/src/main/java/com/syscxp/vpn/host/VpnHost.java
@@ -287,17 +287,15 @@ public class VpnHost extends HostBase implements Host {
 
                     @Override
                     public void run(final FlowTrigger trigger, Map data) {
-                        String srcPath = PathUtil.findFileOnClassPath(String.format("ansible/vpn/%s", agentPackageName),
-                                true).getAbsolutePath();
+                        String srcPath = String.format("%s/files/vpn/%s", AnsibleConstant.ROOT_DIR, agentPackageName);
                         String destPath = String.format("/var/lib/syscxp/vpn/package/%s", agentPackageName);
                         SshFileMd5Checker checker = new SshFileMd5Checker();
                         checker.setUsername(getSelf().getUsername());
                         checker.setPassword(getSelf().getPassword());
                         checker.setSshPort(getSelf().getSshPort());
                         checker.setTargetIp(getSelf().getHostIp());
-                        checker.addSrcDestPair(SshFileMd5Checker.SYSCXPLIB_SRC_PATH, String.format
-                                ("/var/lib/syscxp/vpn/package/%s",
-                                        AnsibleGlobalProperty.SYSCXPLIB_PACKAGE_NAME));
+                        checker.addSrcDestPair(AnsibleConstant.SYSCXPLIB_ROOT + AnsibleGlobalProperty.SYSCXPLIB_PACKAGE_NAME,
+                                String.format("/var/lib/syscxp/vpn/package/%s",AnsibleGlobalProperty.SYSCXPLIB_PACKAGE_NAME));
                         checker.addSrcDestPair(srcPath, destPath);
 
                         AnsibleRunner runner = new AnsibleRunner();
