bf21cf27e715ae4fc1bb543c0bad10752ca12f1e
zhangqiuyu <zhangqiuyu@syscloud.cn>
2017-12-08 15:05:55 +0800

    modify

--- header/src/main/java/com/syscxp/header/tunnel/aliEdgeRouter/AliEdgeRouterVO.java
+++ header/src/main/java/com/syscxp/header/tunnel/aliEdgeRouter/AliEdgeRouterVO.java
@@ -1,13 +1,25 @@
 package com.syscxp.header.tunnel.aliEdgeRouter;
 
+import com.syscxp.header.tunnel.endpoint.EndpointVO;
+import com.syscxp.header.tunnel.tunnel.TunnelEO;
 import com.syscxp.header.vo.EO;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table
 @EO(EOClazz = AliEdgeRouterEO.class)
 public class AliEdgeRouterVO extends AliEdgeRouterAO{
 
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "tunnelUuid", insertable = false, updatable = false)
+    private TunnelEO tunnelEO;
+
+    public TunnelEO getTunnelEO() {
+        return tunnelEO;
+    }
+
+    public void setTunnelEO(TunnelEO tunnelEO) {
+        this.tunnelEO = tunnelEO;
+    }
 }
