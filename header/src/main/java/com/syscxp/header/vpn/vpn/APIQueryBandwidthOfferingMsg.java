db1e242031df6fa49ca1a4b4310b5d9aa92ef3d9
wangjie <717728534@qq.com>
2017-12-18 20:50:17 +0800

    fix

--- header/src/main/java/com/syscxp/header/vpn/vpn/APIQueryBandwidthOfferingMsg.java
+++ header/src/main/java/com/syscxp/header/vpn/vpn/APIQueryBandwidthOfferingMsg.java
@@ -1,11 +1,11 @@
 package com.syscxp.header.vpn.vpn;
 
-import com.syscxp.header.configuration.BandwidthOfferingVO;
+import com.syscxp.header.configuration.BandwidthOfferingInventory;
 import com.syscxp.header.identity.Action;
 import com.syscxp.header.query.APIQueryMessage;
 import com.syscxp.header.query.AutoQuery;
 
 @Action(services = {VpnConstant.ACTION_SERVICE}, category = VpnConstant.ACTION_CATEGORY_VPN, names = {"read"})
-@AutoQuery(replyClass = APIQueryBandwidthOfferingReply.class, inventoryClass = BandwidthOfferingVO.class)
+@AutoQuery(replyClass = APIQueryBandwidthOfferingReply.class, inventoryClass = BandwidthOfferingInventory.class)
 public class APIQueryBandwidthOfferingMsg extends APIQueryMessage {
 }
