73f9dce2097dc4d0f663e4397853469915a3093a
zhangqiuyu <zhangqiuyu@syscloud.cn>
2017-12-26 16:48:00 +0800

    modify

--- header/src/main/java/com/syscxp/header/tunnel/solution/APIDeleteSolutionVpnEvent.java
+++ header/src/main/java/com/syscxp/header/tunnel/solution/APIDeleteSolutionVpnEvent.java
@@ -3,7 +3,7 @@ package com.syscxp.header.tunnel.solution;
 import com.syscxp.header.message.APIEvent;
 import com.syscxp.header.rest.RestResponse;
 
-@RestResponse(allTo = "inventory")
+@RestResponse(allTo = "solutionInventory")
 public class APIDeleteSolutionVpnEvent extends APIEvent {
     private SolutionInventory solutionInventory;
     public APIDeleteSolutionVpnEvent(String apiId) {
