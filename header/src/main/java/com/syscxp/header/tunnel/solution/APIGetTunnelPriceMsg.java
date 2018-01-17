9970e099474026adc31d71fc68abce44395a58fc
zhangqiuyu <zhangqiuyu@syscloud.cn>
2018-01-17 10:47:38 +0800

    方案价格查询的修改

--- header/src/main/java/com/syscxp/header/tunnel/solution/APIGetTunnelPriceMsg.java
+++ header/src/main/java/com/syscxp/header/tunnel/solution/APIGetTunnelPriceMsg.java
@@ -1,26 +1,33 @@
 package com.syscxp.header.tunnel.solution;
 
+import com.syscxp.header.billing.ProductChargeModel;
 import com.syscxp.header.configuration.BandwidthOfferingVO;
 import com.syscxp.header.identity.Action;
 import com.syscxp.header.message.APIParam;
 import com.syscxp.header.message.APISyncCallMessage;
 import com.syscxp.header.tunnel.TunnelConstant;
+import com.syscxp.header.tunnel.endpoint.EndpointVO;
+import com.syscxp.header.tunnel.endpoint.InnerConnectedEndpointVO;
 
 @Action(services = {TunnelConstant.ACTION_SERVICE}, category = SolutionConstant.ACTION_CATEGORY, names = "read")
 public class APIGetTunnelPriceMsg extends APISyncCallMessage {
-    @APIParam(maxLength = 32, resourceType = SolutionTunnelVO.class)
-    private String uuid;
 
     @APIParam(emptyString = false,maxLength = 32,resourceType = BandwidthOfferingVO.class)
     private String bandwidthOfferingUuid;
-
-    public String getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
+    @APIParam(emptyString = false,resourceType = EndpointVO.class)
+    private String endpointAUuid;
+    @APIParam(emptyString = false,resourceType = EndpointVO.class)
+    private String endpointZUuid;
+    @APIParam(emptyString = false,required = false,resourceType = SolutionInterfaceVO.class, checkAccount = true)
+    private String interfaceAUuid;
+    @APIParam(emptyString = false,required = false,resourceType = SolutionInterfaceVO.class, checkAccount = true)
+    private String interfaceZUuid;
+    @APIParam(emptyString = false,required = false,resourceType = InnerConnectedEndpointVO.class)
+    private String innerConnectedEndpointUuid;
+    @APIParam
+    private Integer duration;
+    @APIParam(emptyString = false,validValues = {"BY_MONTH", "BY_YEAR","BY_DAY"})
+    private ProductChargeModel productChargeModel;
 
     public String getBandwidthOfferingUuid() {
         return bandwidthOfferingUuid;
@@ -28,5 +35,61 @@ public class APIGetTunnelPriceMsg extends APISyncCallMessage {
 
     public void setBandwidthOfferingUuid(String bandwidthOfferingUuid) {
         this.bandwidthOfferingUuid = bandwidthOfferingUuid;
+    }
+
+    public String getEndpointAUuid() {
+        return endpointAUuid;
+    }
+
+    public void setEndpointAUuid(String endpointAUuid) {
+        this.endpointAUuid = endpointAUuid;
+    }
+
+    public String getEndpointZUuid() {
+        return endpointZUuid;
+    }
+
+    public void setEndpointZUuid(String endpointZUuid) {
+        this.endpointZUuid = endpointZUuid;
+    }
+
+    public String getInterfaceAUuid() {
+        return interfaceAUuid;
+    }
+
+    public void setInterfaceAUuid(String interfaceAUuid) {
+        this.interfaceAUuid = interfaceAUuid;
+    }
+
+    public String getInterfaceZUuid() {
+        return interfaceZUuid;
+    }
+
+    public void setInterfaceZUuid(String interfaceZUuid) {
+        this.interfaceZUuid = interfaceZUuid;
+    }
+
+    public String getInnerConnectedEndpointUuid() {
+        return innerConnectedEndpointUuid;
+    }
+
+    public void setInnerConnectedEndpointUuid(String innerConnectedEndpointUuid) {
+        this.innerConnectedEndpointUuid = innerConnectedEndpointUuid;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public ProductChargeModel getProductChargeModel() {
+        return productChargeModel;
+    }
+
+    public void setProductChargeModel(ProductChargeModel productChargeModel) {
+        this.productChargeModel = productChargeModel;
     }
 }
--- header/src/main/java/com/syscxp/header/tunnel/solution/APIModifyBandwidthTunnelPriceReply.java
+++ header/src/main/java/com/syscxp/header/tunnel/solution/APIModifyBandwidthTunnelPriceReply.java
@@ -1,9 +1,10 @@
 package com.syscxp.header.tunnel.solution;
 
 import com.syscxp.header.query.APIQueryReply;
+
 import java.math.BigDecimal;
 
-public class APIGetTunnelPriceReply extends APIQueryReply {
+public class APIModifyBandwidthTunnelPriceReply extends APIQueryReply {
     private BigDecimal price;
 
     public BigDecimal getPrice() {
