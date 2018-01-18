c4e5cc0c6db0c95df03a8a43c6525055d63e8a2e
sunxuelong <sunxuelong@syscloud.cn>
2018-01-18 14:38:19 +0800

    update

--- alarm/src/main/java/com/syscxp/alarm/header/log/APIHandleStandardAlarmReply.java
+++
@@ -1,6 +0,0 @@
-package com.syscxp.alarm.header.log;
-
-import com.syscxp.header.message.NeedReplyMessage;
-
-public class APIHandleStandardAlarmReply extends NeedReplyMessage {
-}
--- alarm/src/main/java/com/syscxp/alarm/header/log/APIHandleTunnelALarmReply.java
+++
@@ -1,7 +0,0 @@
-package com.syscxp.alarm.header.log;
-
-import com.syscxp.header.message.NeedReplyMessage;
-
-public class APIHandleTunnelALarmReply extends NeedReplyMessage {
-
-}
