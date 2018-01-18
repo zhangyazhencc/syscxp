47471981ba3a53ace2d38d84b97d2f0d00fdc5f4
dingchunyu <dingchunyu@syscloud.cn>
2018-01-18 16:54:24 +0800

    update

---
+++ header/src/main/java/com/syscxp/header/tunnel/tunnel/ModifyTunnelPortsZKMsg.java
@@ -0,0 +1,28 @@
+package com.syscxp.header.tunnel.tunnel;
+
+import com.syscxp.header.message.NeedReplyMessage;
+
+/**
+ * Create by DCY on 2018/1/18
+ */
+public class ModifyTunnelPortsZKMsg extends NeedReplyMessage {
+    private String taskUuid;
+
+    private String tunnelUuid;
+
+    public String getTaskUuid() {
+        return taskUuid;
+    }
+
+    public void setTaskUuid(String taskUuid) {
+        this.taskUuid = taskUuid;
+    }
+
+    public String getTunnelUuid() {
+        return tunnelUuid;
+    }
+
+    public void setTunnelUuid(String tunnelUuid) {
+        this.tunnelUuid = tunnelUuid;
+    }
+}
