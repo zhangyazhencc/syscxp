697276bb5e873278087d632ebee33bdf6d8e9d9f
dingchunyu <dingchunyu@syscloud.cn>
2018-03-07 17:14:48 +0800

    update

--- tunnel/src/main/java/com/syscxp/tunnel/network/L3NetworkControllerBase.java
+++ tunnel/src/main/java/com/syscxp/tunnel/network/L3NetworkControllerBase.java
@@ -1,16 +1,32 @@
 package com.syscxp.tunnel.network;
 
+import com.syscxp.core.CoreGlobalProperty;
 import com.syscxp.core.cloudbus.CloudBus;
 import com.syscxp.core.db.DatabaseFacade;
 import com.syscxp.core.errorcode.ErrorFacade;
+import com.syscxp.header.billing.ProductChargeModel;
+import com.syscxp.header.core.Completion;
+import com.syscxp.header.errorcode.ErrorCode;
 import com.syscxp.header.message.APIMessage;
 import com.syscxp.header.message.Message;
 import com.syscxp.header.rest.RESTFacade;
+import com.syscxp.header.tunnel.network.CreateL3EndpointMsg;
+import com.syscxp.header.tunnel.network.CreateL3EndpointReply;
+import com.syscxp.header.tunnel.network.L3EndPointVO;
+import com.syscxp.header.tunnel.tunnel.TaskResourceVO;
+import com.syscxp.header.tunnel.tunnel.TaskStatus;
+import com.syscxp.tunnel.sdnController.ControllerCommands;
+import com.syscxp.tunnel.sdnController.ControllerRestConstant;
+import com.syscxp.tunnel.sdnController.ControllerRestFacade;
 import com.syscxp.utils.Utils;
+import com.syscxp.utils.gson.JSONObjectUtil;
 import com.syscxp.utils.logging.CLogger;
 import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 /**
  * Create by DCY on 2018/3/7
@@ -37,8 +53,56 @@ public class L3NetworkControllerBase {
     }
 
     private void handleLocalMessage(Message msg){
-
+        if(msg instanceof CreateL3EndpointMsg){
+            handle((CreateL3EndpointMsg) msg);
+        }else{
+            bus.dealWithUnknownMessage(msg);
+        }
     }
 
     private void handleApiMessage(APIMessage msg){}
+
+    private void handle(CreateL3EndpointMsg msg){
+        CreateL3EndpointReply reply = new CreateL3EndpointReply();
+
+        L3EndPointVO l3EndPointVO = dbf.findByUuid(msg.getL3EndpointUuid(),L3EndPointVO.class);
+        TaskResourceVO taskResourceVO = dbf.findByUuid(msg.getTaskUuid(),TaskResourceVO.class);
+
+        //ControllerCommands.IssuedTunnelCommand issuedTunnelCommand = getTunnelConfigInfo(tunnelVO, false);
+        //String command = JSONObjectUtil.toJsonString(issuedTunnelCommand);
+        String command = "";
+        ControllerRestFacade crf = new ControllerRestFacade(CoreGlobalProperty.CONTROLLER_MANAGER_URL);
+
+        crf.sendCommand(ControllerRestConstant.CREATE_L3ENDPOINT, command, new Completion(null) {
+            @Override
+            public void success() {
+                logger.info("下发创建云网络连接点成功！");
+
+                //修改连接点状态
+
+                //更新任务状态
+                taskResourceVO.setBody(command);
+                taskResourceVO.setStatus(TaskStatus.Success);
+                dbf.updateAndRefresh(taskResourceVO);
+
+                bus.reply(msg, reply);
+            }
+
+            @Override
+            public void fail(ErrorCode errorCode) {
+                logger.info("下发创建云网络连接点失败！");
+
+                //修改连接点状态
+
+                //更新任务状态
+                taskResourceVO.setStatus(TaskStatus.Fail);
+                taskResourceVO.setBody(command);
+                taskResourceVO.setResult(JSONObjectUtil.toJsonString(errorCode));
+                dbf.updateAndRefresh(taskResourceVO);
+
+                reply.setError(errorCode);
+                bus.reply(msg, reply);
+            }
+        });
+    }
 }
--- tunnel/src/main/java/com/syscxp/tunnel/sdnController/ControllerRestConstant.java
+++ tunnel/src/main/java/com/syscxp/tunnel/sdnController/ControllerRestConstant.java
@@ -28,4 +28,13 @@ public interface ControllerRestConstant {
     static final String STOP_TUNNEL_MONITOR_ZK = "/tunnel_monitor/stop_zk";
     static final String MODIFY_TUNNEL_MONITOR = "/tunnel_monitor/modify";
     static final String MODIFY_TUNNEL_MONITOR_ZK = "/tunnel_monitor/modify_zk";
+
+    static final String CREATE_L3ENDPOINT = "/l3net/create";
+    static final String DELETE_L3ENDPOINT = "/l3net/delete";
+    static final String ADD_ROUTES = "/l3net/add_routes";
+    static final String DELETE_ROUTES = "/l3net/del_routes";
+    static final String MODIFY_CONNECT_IP = "/l3net/modify_connect_ip";
+    static final String MODIFY_BANDWIDTH = "/l3net/modify_bandwidth";
+
+
 }
