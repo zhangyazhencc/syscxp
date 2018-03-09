1cb64ca4da70c8e3bd2c4092aa85f69e7361c5b0
赵相辉 <zhaoxh@syscloud.cn>
2018-03-09 09:45:24 +0800

    udpate

--- account/src/main/java/com/syscxp/account/header/log/APIQueryNoticeMsg.java
+++ account/src/main/java/com/syscxp/account/header/log/APIQueryNoticeMsg.java
@@ -2,10 +2,11 @@ package com.syscxp.account.header.log;
 
 import com.syscxp.account.header.account.AccountConstant;
 import com.syscxp.header.identity.Action;
+import com.syscxp.header.identity.SuppressUserCredentialCheck;
 import com.syscxp.header.query.APIQueryMessage;
 import com.syscxp.header.query.AutoQuery;
 
-@Action(services = {AccountConstant.ACTION_SERVICE}, category = AccountConstant.ACTION_CATEGORY_ACCOUNT, names = {"read"})
+@SuppressUserCredentialCheck
 @AutoQuery(replyClass = APIQueryNoticeReply.class, inventoryClass = NoticeInventory.class)
 public class APIQueryNoticeMsg extends APIQueryMessage {
 }
