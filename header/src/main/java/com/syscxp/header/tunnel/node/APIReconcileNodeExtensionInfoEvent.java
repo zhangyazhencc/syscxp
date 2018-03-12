726e18a292011221be627d6a894f15e02a8cdb80
wangjie <wangjie@syscloud.cn>
2018-03-12 18:16:26 +0800

    update rest tunnel

--- header/src/main/java/com/syscxp/header/tunnel/node/APIReconcileNodeExtensionInfoEvent.java
+++ header/src/main/java/com/syscxp/header/tunnel/node/APIReconcileNodeExtensionInfoEvent.java
@@ -6,7 +6,6 @@ import com.syscxp.header.rest.RestResponse;
 /**
  * Created by wangwg on 2017/12/29
  */
-@RestResponse(allTo = "inventory")
 public class APIReconcileNodeExtensionInfoEvent extends APIEvent {
 
     public APIReconcileNodeExtensionInfoEvent(String apiId) {
