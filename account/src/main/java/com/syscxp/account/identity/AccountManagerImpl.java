c0c37a4fc813a3d7f773d3c81d12deca0422c773
wangjie <wangjie@syscloud.cn>
2018-03-23 17:38:45 +0800

    fix

--- account/src/main/java/com/syscxp/account/identity/AccountManagerImpl.java
+++ account/src/main/java/com/syscxp/account/identity/AccountManagerImpl.java
@@ -186,20 +186,9 @@ public class AccountManagerImpl extends AbstractService implements AccountManage
     private void handle(APILogInBySecretIdMsg msg) {
         APILogInBySecretIdReply reply = new APILogInBySecretIdReply();
 
-        SimpleQuery<AccountApiSecurityVO> q = dbf.createQuery(AccountApiSecurityVO.class);
-        q.add(AccountApiSecurityVO_.secretId, SimpleQuery.Op.EQ, msg.getSecretId());
-        q.add(AccountApiSecurityVO_.secretKey, SimpleQuery.Op.EQ, msg.getSecretKey());
-        AccountApiSecurityVO vo = q.find();
-        if (vo != null && (vo.getAllowIp() == null ||
-                vo.getAllowIp().trim().equals("") || vo.getAllowIp().contains(msg.getIP()))) {
-            AccountApiSecurityVO api = q.find();
-            reply.setSessionUuid(identiyInterceptor.initSession(
-                    dbf.findByUuid(api.getAccountUuid(), AccountVO.class), null).getUuid());
+        String sessionUuid = identiyInterceptor.getSessionUuid(msg.getSecretId(), msg.getSecretKey(), msg.getIP());
 
-        } else {
-            reply.setError(errf.instantiateErrorCode(IdentityErrors.AUTHENTICATION_ERROR,
-                    "Incorrect secretId or secretKey or illegal ip"));
-        }
+        reply.setSessionUuid(sessionUuid);
 
         bus.reply(msg, reply);
     }
