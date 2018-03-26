8edcb42d9a2c935f13dc8653ecd587888a533bf5
wangwg <wangwg@syscloud.cn>
2018-03-26 09:56:07 +0800

    update

--- trustee/src/main/java/com/syscxp/trustee/trustee/TrusteeApiInterceptor.java
+++ trustee/src/main/java/com/syscxp/trustee/trustee/TrusteeApiInterceptor.java
@@ -4,7 +4,7 @@ import com.syscxp.header.apimediator.ApiMessageInterceptionException;
 import com.syscxp.header.apimediator.ApiMessageInterceptor;
 import com.syscxp.header.message.APIMessage;
 
-public class TrusteeInterceptor implements ApiMessageInterceptor {
+public class TrusteeApiInterceptor implements ApiMessageInterceptor {
     @Override
     public APIMessage intercept(APIMessage msg) throws ApiMessageInterceptionException {
         return msg;
