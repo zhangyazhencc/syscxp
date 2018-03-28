6abba23c41e373dbd193be21c18f2f70e37560ad
sunxuelong <sunxuelong@syscloud.cn>
2018-03-28 15:32:38 +0800

    L3 Network Monitor

--- tunnel/src/main/java/com/syscxp/tunnel/monitor/L3NetworkMonitorManager.java
+++ tunnel/src/main/java/com/syscxp/tunnel/monitor/L3NetworkMonitorManager.java
@@ -1,8 +1,5 @@
 package com.syscxp.tunnel.monitor;
 
-import com.syscxp.header.tunnel.monitor.APIStartL3NetworkMonitorEvent;
-import com.syscxp.header.tunnel.monitor.APIStartL3NetworkMonitorMsg;
-
 /**
  * @Author: sunxuelong.
  * @Cretion Date: 2018-03-22.
