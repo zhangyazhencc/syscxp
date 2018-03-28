614277e4c448fa2f807a104d682b154ddebb965d
赵相辉 <zhaoxh@syscloud.cn>
2018-03-28 18:46:09 +0800

    update

--- alarm/conf/db/V0.2__schema.sql
+++ alarm/conf/db/V0.2__schema.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `syscxp_alarm`.`ContactVO` CHANGE `lastOpDate` `lastOpDate` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
-ALTER TABLE `syscxp_alarm`.`ContactVO` CHANGE `createDate` `createDate` TIMESTAMP NULL DEFAULT NULL;
+ALTER TABLE `syscxp_alarm`.`ContactVO` CHANGE `createDate` `createDate` TIMESTAMP default CURRENT_TIMESTAMP;
 
 ALTER TABLE `syscxp_alarm`.`ResourcePolicyRefVO` CHANGE `lastOpDate` `lastOpDate` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
-ALTER TABLE `syscxp_alarm`.`ResourcePolicyRefVO` CHANGE `createDate` `createDate` TIMESTAMP NULL DEFAULT NULL;
+ALTER TABLE `syscxp_alarm`.`ResourcePolicyRefVO` CHANGE `createDate` `createDate` TIMESTAMP default CURRENT_TIMESTAMP;
 
