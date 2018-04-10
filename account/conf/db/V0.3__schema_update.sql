b62175fa5440ce77dd5224884339b06af0432fa4
wangwg <wangwg@syscloud.cn>
2018-04-10 17:26:11 +0800

    update

---
+++ account/conf/db/V0.3__schema_update.sql
@@ -0,0 +1,3 @@
+INSERT INTO TicketTypeVO (uuid, name, category) VALUES ('cloudHub','云网通','officialWebsite');
+INSERT INTO TicketTypeVO (uuid, name, category) VALUES ('consult','专业咨询','officialWebsite');
+INSERT INTO TicketTypeVO (uuid, name, category) VALUES ('applyProxy','申请合作伙伴','officialWebsite');
