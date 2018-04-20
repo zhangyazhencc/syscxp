74e8b8e6c75946fb313b575c2fedd01010fb23d6
sunxuelong <sunxuelong@syscloud.cn>
2018-04-20 17:01:55 +0800

    update

--- header/src/main/java/com/syscxp/header/tunnel/monitor/APIQueryL3NetworkMonitorMsg.java
+++ header/src/main/java/com/syscxp/header/tunnel/monitor/APIQueryL3NetworkMonitorMsg.java
@@ -3,6 +3,7 @@ package com.syscxp.header.tunnel.monitor;
 import com.syscxp.header.identity.Action;
 import com.syscxp.header.message.APIParam;
 import com.syscxp.header.message.APISyncCallMessage;
+import com.syscxp.header.query.APIQueryMessage;
 import com.syscxp.header.query.AutoQuery;
 import com.syscxp.header.tunnel.MonitorConstant;
 import com.syscxp.header.tunnel.TunnelConstant;
@@ -15,16 +16,7 @@ import com.syscxp.header.tunnel.network.L3EndpointVO;
  */
 
 @Action(services = {TunnelConstant.ACTION_SERVICE}, category = MonitorConstant.ACTION_CATEGORY, names = {"read"})
-public class APIQueryL3NetworkMonitorMsg extends APISyncCallMessage {
-    @APIParam(emptyString = false,resourceType = L3EndpointVO.class)
-    private String l3EndpointUuid;
-
-    public String getL3EndpointUuid() {
-        return l3EndpointUuid;
-    }
-
-    public void setL3EndpointUuid(String l3EndpointUuid) {
-        this.l3EndpointUuid = l3EndpointUuid;
-    }
+@AutoQuery(replyClass = APIQueryL3NetworkMonitorReply.class,inventoryClass = L3NetworkMonitorInventory.class)
+public class APIQueryL3NetworkMonitorMsg extends APIQueryMessage {
 
 }
--- header/src/main/java/com/syscxp/header/tunnel/monitor/APIQueryL3NetworkMonitorReply.java
+++ header/src/main/java/com/syscxp/header/tunnel/monitor/APIQueryL3NetworkMonitorReply.java
@@ -2,19 +2,31 @@ package com.syscxp.header.tunnel.monitor;
 
 import com.syscxp.header.query.APIQueryReply;
 
+import java.util.List;
+
 /**
  * @Author: sunxuelong.
  * @Cretion Date: 2018-4-8.
  * @Description: .
  */
 public class APIQueryL3NetworkMonitorReply extends APIQueryReply {
-    private L3EndpointMonitorInventory inventory;
+    private L3NetworkMonitorInventory inventory;
 
-    public L3EndpointMonitorInventory getInventory() {
+    private List<L3NetworkMonitorInventory> inventories;
+    
+    public L3NetworkMonitorInventory getInventory() {
         return inventory;
     }
 
-    public void setInventory(L3EndpointMonitorInventory inventory) {
+    public void setInventory(L3NetworkMonitorInventory inventory) {
         this.inventory = inventory;
+    }
+
+    public List<L3NetworkMonitorInventory> getInventories() {
+        return inventories;
+    }
+
+    public void setInventories(List<L3NetworkMonitorInventory> inventories) {
+        this.inventories = inventories;
     }
 }
