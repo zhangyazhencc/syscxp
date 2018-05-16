0bda176f3482ac6a50ddb9aa8dd91c8804b1be41
yunling <yunling@syscloud.cn>
2018-05-16 14:39:30 +0800

    update

--- header/src/main/java/com/syscxp/header/host/APIQueryHostMsg.java
+++ header/src/main/java/com/syscxp/header/host/APIQueryHostMsg.java
@@ -4,13 +4,12 @@ import com.syscxp.header.identity.Action;
 import com.syscxp.header.query.APIQueryMessage;
 import com.syscxp.header.query.AutoQuery;
 import com.syscxp.header.tunnel.TunnelConstant;
-import com.syscxp.header.vpn.vpn.VpnConstant;
 
 import java.util.List;
 
 import static java.util.Arrays.asList;
 
-@Action(services = {TunnelConstant.ACTION_SERVICE, VpnConstant.ACTION_SERVICE}, category = HostConstant.ACTION_CATEGORY, names = {"read"}, adminOnly = true)
+@Action(services = {HostConstant.TUNNEL_ACTION_SERVICE, HostConstant.VPN_ACTION_SERVICE}, category = HostConstant.ACTION_CATEGORY, names = {"read"}, adminOnly = true)
 @AutoQuery(replyClass = APIQueryHostReply.class, inventoryClass = HostInventory.class)
 public class APIQueryHostMsg extends APIQueryMessage {
 
