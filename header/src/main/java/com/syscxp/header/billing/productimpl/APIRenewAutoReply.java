b59430f636440c9ff54b6925fc8c37db167a042f
赵相辉 <zhaoxh@syscloud.cn>
2018-05-17 10:53:33 +0800

    update

--- header/src/main/java/com/syscxp/header/billing/productimpl/APIRenewAutoReply.java
+++ header/src/main/java/com/syscxp/header/billing/productimpl/APIRenewAutoReply.java
@@ -3,11 +3,25 @@ package com.syscxp.header.billing.productimpl;
 import com.syscxp.header.billing.ProductType;
 import com.syscxp.header.message.APIReply;
 
+import java.sql.Timestamp;
+
 public class APIRenewAutoReply extends APIReply {
 
     private ProductType productType;
 
     private String productUuid;
+
+    private Timestamp expireDate;
+
+    private boolean temporaryRenew;
+
+    public boolean isTemporaryRenew() {
+        return temporaryRenew;
+    }
+
+    public void setTemporaryRenew(boolean temporaryRenew) {
+        this.temporaryRenew = temporaryRenew;
+    }
 
     public ProductType getProductType() {
         return productType;
@@ -23,5 +37,13 @@ public class APIRenewAutoReply extends APIReply {
 
     public void setProductUuid(String productUuid) {
         this.productUuid = productUuid;
+    }
+
+    public Timestamp getExpireDate() {
+        return expireDate;
+    }
+
+    public void setExpireDate(Timestamp expireDate) {
+        this.expireDate = expireDate;
     }
 }
