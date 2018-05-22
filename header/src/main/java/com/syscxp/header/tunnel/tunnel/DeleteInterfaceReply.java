1427bba51fabbe9a051e29837a701ad9cfd41b03
dingchunyu <dingchunyu@syscloud.cn>
2018-05-22 17:07:08 +0800

    update

---
+++ header/src/main/java/com/syscxp/header/tunnel/tunnel/DeleteInterfaceReply.java
@@ -0,0 +1,9 @@
+package com.syscxp.header.tunnel.tunnel;
+
+import com.syscxp.header.message.MessageReply;
+
+/**
+ * Create by DCY on 2018/5/22
+ */
+public class DeleteInterfaceReply extends MessageReply {
+}
