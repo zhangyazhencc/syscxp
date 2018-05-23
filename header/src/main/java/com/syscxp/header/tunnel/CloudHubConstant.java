611aab7273b9c7a41b4401ef78a57ee56e420b35
dingchunyu <dingchunyu@syscloud.cn>
2018-05-23 09:10:21 +0800

    update

--- header/src/main/java/com/syscxp/header/tunnel/CloudHubConstant.java
+++
@@ -1,9 +0,0 @@
-package com.syscxp.header.tunnel;
-
-import com.syscxp.header.configuration.PythonClass;
-
-@PythonClass
-public interface CloudHubConstant {
-    String SERVICE_ID = "cloudHub";
-    String ACTION_CATEGORY = "cloudHub";
-}
