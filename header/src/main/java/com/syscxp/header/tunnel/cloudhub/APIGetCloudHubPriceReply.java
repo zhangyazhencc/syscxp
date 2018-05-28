95ceb6fb851cda12b4edffbff2fcde2ee59758af
dingchunyu <dingchunyu@syscloud.cn>
2018-05-28 16:10:58 +0800

    update

---
+++ header/src/main/java/com/syscxp/header/tunnel/cloudhub/APIGetCloudHubPriceReply.java
@@ -0,0 +1,15 @@
+package com.syscxp.header.tunnel.cloudhub;
+
+import com.syscxp.header.billing.APIGetProductPriceReply;
+
+/**
+ * Create by DCY on 2018/5/28
+ */
+public class APIGetCloudHubPriceReply extends APIGetProductPriceReply {
+    public APIGetCloudHubPriceReply() {
+    }
+
+    public APIGetCloudHubPriceReply(APIGetProductPriceReply reply) {
+        super(reply);
+    }
+}
---
+++ header/src/main/java/com/syscxp/header/tunnel/cloudhub/APIGetRenewCloudHubPriceReply.java
@@ -0,0 +1,14 @@
+package com.syscxp.header.tunnel.cloudhub;
+
+import com.syscxp.header.billing.APIGetRenewProductPriceReply;
+
+/**
+ * Create by DCY on 2018/5/28
+ */
+public class APIGetRenewCloudHubPriceReply extends APIGetRenewProductPriceReply {
+    public APIGetRenewCloudHubPriceReply(){}
+
+    public APIGetRenewCloudHubPriceReply(APIGetRenewProductPriceReply reply) {
+        super(reply);
+    }
+}
