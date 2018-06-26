bb30edbb86e7d6dc204272ec343759f6bbac9ed1
wangjie <wangjie@syscloud.cn>
2018-06-26 08:36:56 +0800

    update

--- ecp-sdk/src/main/java/com/syscxp/kvm/KVMHostInventory.java
+++ ecp-sdk/src/main/java/com/syscxp/kvm/KVMHostInventory.java
@@ -3,14 +3,22 @@ package com.syscxp.kvm;
 public class KVMHostInventory {
 
     private String zoneUuid;
+    private String zoneName;
+    private String name;
     private String uuid;
+    private String clusterUuid;
     private String managementIp;
+    private String hypervisorType;
+    private String state;
     private String status;
+    private Long totalMemoryCapacity;
     private String publicPhysicalInterface;
     private String privatePhysicalInterface;
     private String tunnelPhysicalInterface;
     private String username;
     private Integer sshPort;
+    private String hostType;
+    private String clusterType;
 
     public String getZoneUuid() {
         return zoneUuid;
@@ -18,6 +26,22 @@ public class KVMHostInventory {
 
     public void setZoneUuid(String zoneUuid) {
         this.zoneUuid = zoneUuid;
+    }
+
+    public String getZoneName() {
+        return zoneName;
+    }
+
+    public void setZoneName(String zoneName) {
+        this.zoneName = zoneName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getUuid() {
@@ -28,6 +52,14 @@ public class KVMHostInventory {
         this.uuid = uuid;
     }
 
+    public String getClusterUuid() {
+        return clusterUuid;
+    }
+
+    public void setClusterUuid(String clusterUuid) {
+        this.clusterUuid = clusterUuid;
+    }
+
     public String getManagementIp() {
         return managementIp;
     }
@@ -36,12 +68,36 @@ public class KVMHostInventory {
         this.managementIp = managementIp;
     }
 
+    public String getHypervisorType() {
+        return hypervisorType;
+    }
+
+    public void setHypervisorType(String hypervisorType) {
+        this.hypervisorType = hypervisorType;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
     public String getStatus() {
         return status;
     }
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public Long getTotalMemoryCapacity() {
+        return totalMemoryCapacity;
+    }
+
+    public void setTotalMemoryCapacity(Long totalMemoryCapacity) {
+        this.totalMemoryCapacity = totalMemoryCapacity;
     }
 
     public String getPublicPhysicalInterface() {
@@ -82,5 +138,21 @@ public class KVMHostInventory {
 
     public void setSshPort(Integer sshPort) {
         this.sshPort = sshPort;
+    }
+
+    public String getHostType() {
+        return hostType;
+    }
+
+    public void setHostType(String hostType) {
+        this.hostType = hostType;
+    }
+
+    public String getClusterType() {
+        return clusterType;
+    }
+
+    public void setClusterType(String clusterType) {
+        this.clusterType = clusterType;
     }
 }
